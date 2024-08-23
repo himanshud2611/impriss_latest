4a4657a756199411e103321e6aa2802eadd5ae0c
himanshud2611 <hdubey261102@gmail.com>
2024-08-24 02:56:33 +0530

    initial commit

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-#from app import app
-
-#if __name__ == "__main__":
-#    app.config['ENV']="development"
-    #app.run(host='0.0.0.0', port=5000)
-#    app.run(debug=True)
-
 from app import app
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.config['ENV']="development"
+    
+    app.run(debug=True)
